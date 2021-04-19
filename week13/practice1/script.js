6129e62316de44ae6229fd45a39724906ffec9d2
LilyMaxutova <lilya.maxutova@gmail.com>
2021-04-19 22:38:47 +0300

    week13

---
+++ week13/practice1/script.js
@@ -0,0 +1,6 @@
+function user() {
+  let name = prompt('Как Вас зовут?');
+  alert(`Привет, ${name}`);
+}
+  
+
---
+++ week13/practice2/script.js
@@ -0,0 +1,23 @@
+function sum() {
+  let a = prompt('Введите число');
+  let b = prompt('Введите число');
+  alert(Number(a)+Number(b));
+}
+
+function sub() {
+  let a = prompt('Введите число');
+  let b = prompt('Введите число');
+  alert(Number(a)-Number(b));
+}
+
+function multi() {
+  let a = prompt('Введите число');
+  let b = prompt('Введите число');
+  alert(Number(a)*Number(b));
+}
+
+function div() {
+  let a = prompt('Введите число');
+  let b = prompt('Введите число');
+  alert(Number(a)/Number(b));
+}
